29b6ef57d51fceccd94139d67c58d75046cadf21
Toukom's <tokiniaina090605@gmail.com>
2024-07-15 16:09:52 +0300

    feat: change source code link target to '_blank'.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,17 @@
-import Link from 'next/link'
-import React from 'react'
-import { SuperLink } from './SuperLink'
+import Link from "next/link";
+import React from "react";
+import { SuperLink } from "./SuperLink";
 
 const Footer = () => {
   return (
-    <div className='self-end pb-4 mt-8 text-sm text-center text-gray-500'>© 2024 Designed and Developed by Tokiniaina - Source code available <SuperLink href="https://github.com/Toukoms/portfolio">here</SuperLink>.</div>
-  )
-}
+    <div className="self-end pb-4 mt-8 text-sm text-center text-gray-500">
+      © 2024 Designed and Developed by Tokiniaina - Source code available{" "}
+      <SuperLink href="https://github.com/Toukoms/portfolio" target="_blank">
+        here
+      </SuperLink>
+      .
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/SuperLink.tsx
+++ src/components/SuperLink.tsx
@@ -1,10 +1,22 @@
-import Link from 'next/link'
-import React from 'react'
-import { Button } from './ui/button'
+import Link, { LinkProps } from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
 
-export const SuperLink = ({href, children}:{href: string, children:React.ReactNode}) => {
+export const SuperLink = ({
+  href,
+  target,
+  children,
+  ...props
+}: React.PropsWithChildren<LinkProps & { target: string }>) => {
   return (
-    // TODO: change the default active and focus style to just border bottom
-    <Button variant={"link"} className="p-0 focus-visible:ring-0 focus-visible:rounded-none focus-visible:inline focus-visible:border-b-2 border-primary" asChild><Link href={href}>{children}</Link></Button>
-  )
-}
+    <Button
+      variant={"link"}
+      className="p-0 focus-visible:ring-0 focus-visible:rounded-none focus-visible:inline focus-visible:border-b-2 border-primary"
+      asChild
+    >
+      <Link {...props} href={href} target={target}>
+        {children}
+      </Link>
+    </Button>
+  );
+};
